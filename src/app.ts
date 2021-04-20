86b8714dd358344a0a52b414f21259ab5fd62c42
Antony <butterspower@gmail.com>
2021-04-20 23:52:59 +0300

    support for angular

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import Fastify, { FastifyInstance } from 'fastify'
+import fastify from 'fastify'
 import fastifyCompress from 'fastify-compress'
 import fastifyCors from 'fastify-cors'
 import fastifyHelmet from 'fastify-helmet'
@@ -7,14 +7,25 @@ import path from 'path'
 import healthRoute from './routes/health.route'
 import testRoute from './routes/test.route'
 
-const server: FastifyInstance = Fastify({
-    logger: true
+const server = fastify({
+  logger: true
 })
 
 server.register(fastifyCompress)
 server.register(fastifyCors, { origin: '*', exposedHeaders: ['*'] })
 server.register(fastifyHelmet, { contentSecurityPolicy: false })
 server.register(fastifyStatic, { root: path.join(__dirname, 'ui') })
+server.setNotFoundHandler((request, reply) => {
+  if (request.url.includes('/api')) {
+    reply.status(404).send({
+      message: `Route ${request.method}:${request.url} not found`,
+      error: 'Not Found',
+      statusCode: 404
+    })
+  } else {
+    reply.sendFile('index.html')
+  }
+})
 
 // Routes
 server.register(healthRoute, { prefix: '/api/v1' })
@@ -22,7 +33,7 @@ server.register(testRoute, { prefix: '/api/v1' })
 
 const start = async () => {
   try {
-    await server.listen(3000)
+    await server.listen(8080)
   } catch (err) {
     server.log.error(err)
     process.exit(1)
