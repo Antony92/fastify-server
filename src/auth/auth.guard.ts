9021a7bfc19ea356d8e5c97226e3446c8d2859ae
aasenov <aasenov@axway.com>
2021-04-24 01:50:05 +0300

    format

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -3,25 +3,25 @@ import { hasRole } from '../helpers/user.helper'
 import { getJWTFromRequest, isJWTValid } from './jwt.auth'
 
 export const secured = (roles?: string[]) => {
-    return async (request: FastifyRequest, reply: FastifyReply) => {
-        const jwt = getJWTFromRequest(request)
-        const jwtValid = await isJWTValid(jwt)
-        const inRole = jwtValid && hasRole(request, roles)
-        if (!jwtValid) {
-            reply.status(401).send({
-                message: `Token not valid`,
-                error: 'Token error',
-                statusCode: 401,
-            })
-            return reply
-        }
-        if (!inRole) {
-            reply.status(403).send({
-                message: `Access denied`,
-                error: 'Access error',
-                statusCode: 403,
-            })
-            return reply
-        }
-    }
+	return async (request: FastifyRequest, reply: FastifyReply) => {
+		const jwt = getJWTFromRequest(request)
+		const jwtValid = await isJWTValid(jwt)
+		const inRole = jwtValid && hasRole(request, roles)
+		if (!jwtValid) {
+			reply.status(401).send({
+				message: `Token not valid`,
+				error: 'Token error',
+				statusCode: 401,
+			})
+			return reply
+		}
+		if (!inRole) {
+			reply.status(403).send({
+				message: `Access denied`,
+				error: 'Access error',
+				statusCode: 403,
+			})
+			return reply
+		}
+	}
 }
