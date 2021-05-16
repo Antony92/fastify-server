282ff84781c814798ee842d401c6f621895c0d95
Antony <butterspower@gmail.com>
2021-05-16 17:32:25 +0300

    Update test.route.ts

--- src/routes/test.route.ts
+++ src/routes/test.route.ts
@@ -6,8 +6,8 @@ import { testSchemaBody } from '../schema/test.schema'
 const testRoute = async (fastify: FastifyInstance) => {
     fastify.get('/test/get/:id', testGetHandler)
     fastify.post('/test/post', { schema: testSchemaBody }, testPostHandler)
-    fastify.get('/test/secured', { onRequest: secured() }, testSecuredHandler)
-    fastify.get('/test/admin', { onRequest: secured(['admin'])}, testSecuredHandler)
+    fastify.get('/test/secured', { preHandler: secured() }, testSecuredHandler)
+    fastify.get('/test/admin', { preHandler: secured(['admin'])}, testSecuredHandler)
     fastify.get('/test/event', testEventHandler)
 }
 
