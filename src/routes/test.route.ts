455341016d932dc27fcc88bd228278b3947aab90
Antony <butterspower@gmail.com>
2022-04-16 14:15:35 +0300

    fixed role

--- src/routes/test.route.ts
+++ src/routes/test.route.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify'
 import { testGetHandler, testPostHandler, testSecuredHandler, testEventHandler, testGetByIdHandler, testUploadHandler } from '../controllers/test.controller'
 import { secured } from '../auth/auth.guard'
 import { testGetByIdSchema, testGetSchema, testSecuredSchema, testPostSchema, testAdminSchema, testUploadSchema } from '../schema/test.schema'
+import { Roles } from '../models/user.model'
 
 const testRoute = async (fastify: FastifyInstance) => {
 	fastify.get('/test', { schema: testGetSchema }, testGetHandler)
@@ -9,7 +10,7 @@ const testRoute = async (fastify: FastifyInstance) => {
 	fastify.post('/test', { schema: testPostSchema }, testPostHandler)
 	fastify.post('/test/upload', { schema: testUploadSchema, compress: false }, testUploadHandler)
 	fastify.get('/test/secured', { onRequest: secured(), schema: testSecuredSchema }, testSecuredHandler)
-	fastify.get('/test/admin', { onRequest: secured(['admin']), schema: testAdminSchema }, testSecuredHandler)
+	fastify.get('/test/admin', { onRequest: secured([Roles.ADMIN]), schema: testAdminSchema }, testSecuredHandler)
 	fastify.get('/test/event', testEventHandler)
 }
 
