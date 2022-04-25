2da6cacdeee088ebae3ab26bea24f34c4a263672
aasenov <aasenov@axway.com>
2022-04-25 14:43:24 +0300

    some test updates

--- src/tests/main.test.ts
+++ src/tests/main.test.ts
@@ -1,8 +1,11 @@
 import server from '../server'
 
+server.log.level = 'silent'
+
 const test = async () => {
 	const req = await server.inject({ method: 'GET', url: '/api/v1/health' })
-    console.log(req)
+    const res = await req.json()
+    console.log(res)
     server.close()
 }
 
