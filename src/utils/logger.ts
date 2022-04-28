fee7a29e5fddcd71cbc16432a7305b34bb3c6e10
Antony <butterspower@gmail.com>
2022-04-28 16:03:18 +0300

    fix log init

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,6 +1,7 @@
 import pino from 'pino'
+import config from '../config'
 
-const isProduction = process.env.NODE_ENV === 'production'
+const isProduction = process.env.NODE_ENV === 'production' || config.environment === 'production'
 
 const multistream = pino.multistream(
 	[
