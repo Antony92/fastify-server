82aaf5ccd64b8410e434601996749d4269c2ad04
Antony <butterspower@gmail.com>
2022-04-29 19:09:42 +0300

    removed all host

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import server from './server'
 
 const start = async () => {
 	try {
-		await server.listen(config.server.port, '0.0.0.0')
+		await server.listen(config.server.port)
 	} catch (err) {
 		server.log.error(err)
 		process.exit(1)
