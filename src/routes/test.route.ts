7c4b8873a379285949b4801033603833f690d7ee
Antony <butterspower@gmail.com>
2022-07-14 21:37:18 +0300

    hide event schema from swagger

--- src/routes/test.route.ts
+++ src/routes/test.route.ts
@@ -1,5 +1,12 @@
 import { FastifyInstance } from 'fastify'
-import { testGetHandler, testPostHandler, testSecuredHandler, testEventHandler, testGetByIdHandler, testUploadHandler } from '../controllers/test.controller'
+import {
+	testGetHandler,
+	testPostHandler,
+	testSecuredHandler,
+	testEventHandler,
+	testGetByIdHandler,
+	testUploadHandler,
+} from '../controllers/test.controller'
 import { secured } from '../auth/auth.guard'
 import { testGetByIdSchema, testGetSchema, testSecuredSchema, testPostSchema, testAdminSchema, testUploadSchema } from '../schema/test.schema'
 import { Roles } from '../models/user.model'
@@ -11,7 +18,7 @@ const testRoute = async (fastify: FastifyInstance) => {
 	fastify.post('/test/upload', { schema: testUploadSchema }, testUploadHandler)
 	fastify.get('/test/secured', { onRequest: secured(), schema: testSecuredSchema }, testSecuredHandler)
 	fastify.get('/test/admin', { onRequest: secured([Roles.ADMIN]), schema: testAdminSchema }, testSecuredHandler)
-	fastify.get('/test/event', testEventHandler)
+	fastify.get('/test/event', { schema: { hide: true } }, testEventHandler)
 }
 
 export default testRoute
