e98321073b74116ad3e55b93f847fa3dac9f5325
Antony <butterspower@gmail.com>
2022-10-13 18:49:07 +0300

    fix role check

--- src/helpers/user.helper.ts
+++ src/helpers/user.helper.ts
@@ -1,5 +1,5 @@
 import { User } from '../models/user.model'
 
 export const hasRole = (user: User, roles: string[]) => {
-	return user?.roles.some((role) => roles?.includes(role)) ?? false
+	return user?.roles?.some((role) => roles?.includes(role)) ?? false
 }
