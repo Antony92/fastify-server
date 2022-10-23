6fa3eab4501a0fd60432778fb4cea80d995dc45f
Antony <butterspower@gmail.com>
2022-10-23 17:36:54 +0300

    fix

--- src/helpers/user.helper.ts
+++ src/helpers/user.helper.ts
@@ -3,9 +3,3 @@ import { User } from '../models/user.model'
 export const hasRole = (user: User, roles: string[]) => {
 	return user?.roles?.some((role) => roles?.includes(role)) ?? false
 }
-
-export const parseJWT = (token: string) => {
-	const base64Payload = token.split('.')[1]
-	const payload = Buffer.from(base64Payload, 'base64')
-	return JSON.parse(payload.toString())
-}
