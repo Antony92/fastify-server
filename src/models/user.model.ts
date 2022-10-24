6bd2b64015e3136e0ac4cea0f4ad9865fe48e1c1
Antony <butterspower@gmail.com>
2022-10-25 00:43:20 +0300

    fixes

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -14,15 +14,3 @@ export interface UserToken extends User {
 export enum Roles {
 	ADMIN = 'admin'
 }
-
-declare module '@fastify/jwt' {
-	interface FastifyJWT {
-		user: UserToken
-	}
-}
-
-declare module 'fastify' {
-  interface FastifyInstance {
-    microsoftOAuth: OAuth2Namespace
-  }
-}
