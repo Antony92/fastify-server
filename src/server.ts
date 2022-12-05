870d438f340952d56626c9f644dfec54318746f9
Antony <butterspower@gmail.com>
2022-12-05 19:11:11 +0200

    rate limit fix until version update

--- src/server.ts
+++ src/server.ts
@@ -60,7 +60,7 @@ await server.register(import('@fastify/cors'), {
 	credentials: true,
 })
 await server.register(import('@fastify/helmet'), { contentSecurityPolicy: false })
-// await server.register(import('@fastify/rate-limit'), { max: config.server.rateLimit, timeWindow: '15 minutes' })
+await server.register(import('@fastify/rate-limit') as any, { max: config.server.rateLimit, timeWindow: '15 minutes' })
 await server.register(import('@fastify/static'), { root: path.join(__dirname, 'public') })
 await server.register(import('@fastify/multipart'), { limits: { fileSize: 2 * 1024 * 1024 } })
 await server.register(import('@fastify/swagger'), swaggerOptions)
