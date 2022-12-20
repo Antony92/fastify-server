6e3548c9abbdcc8cd640b14223b5a3e379d22fb6
Antony <butterspower@gmail.com>
2022-12-20 15:33:25 +0200

    fix

--- src/test/main.ts
+++ src/test/main.ts
@@ -1,9 +1,2 @@
-import { getServerEventsObservable, sendServerEvent } from '../services/server-event.service.js'
 
-const test = async () => {
-	getServerEventsObservable().subscribe(event => console.log(1, event))
-	sendServerEvent({ message: 'ola', type: 'info'})
-	getServerEventsObservable().subscribe(event => console.log(2, event))
-}
 
-test()
