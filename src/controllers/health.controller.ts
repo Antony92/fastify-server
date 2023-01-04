f5e6d2ee685c6cafa1fa5d6d62b58b81404f5efb
Antony <butterspower@gmail.com>
2023-01-05 00:13:42 +0200

    added prisma stuff

--- src/controllers/health.controller.ts
+++ src/controllers/health.controller.ts
@@ -1,14 +1,18 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import os from 'os'
+import { isPrismaActive } from '../db/prisma.js'
 import { formatBytes, secondsToElapsedTime } from '../helpers/format.helper.js'
 
 export const checkHealthHandler = async (request: FastifyRequest, reply: FastifyReply) => {
 	const { seconds, minutes, hours } = secondsToElapsedTime(os.uptime())
+
+	const dbActive = await isPrismaActive()
 	
 	const serverInfo = {
 		uptime: `${hours}:${minutes}:${seconds}`,
 		totalmem: formatBytes(os.totalmem()),
 		freemem: formatBytes(os.freemem()),
+		database: dbActive
 	}
 	return serverInfo
 }
--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -2,4 +2,18 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
+export const isPrismaActive = async () => {
+    try {
+        await prisma.$queryRaw`SELECT 1`
+        return true
+    } catch (error) {
+        return false
+    }
+}
+
+export const prismaMetrics = async () => {
+    const metrics = await prisma.$metrics.json()
+    return metrics
+}
+
 export default prisma
