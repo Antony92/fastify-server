ff03ea505213e010194023af5f7c323d0c29fa38
Antony <butterspower@gmail.com>
2023-01-21 20:58:09 +0200

    up

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { fastify } from 'fastify'
+import fastify from 'fastify'
 import fastifyCompress from '@fastify/compress'
 import fastifyCookie from '@fastify/cookie'
 import fastifyCors from '@fastify/cors'
