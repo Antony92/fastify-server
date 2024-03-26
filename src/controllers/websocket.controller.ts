73978912d9db1dece8548a72acebedb4872e4a8d
Antony92 <butterspower@gmail.com>
2024-03-26 19:43:42 +0200

    fix

--- src/controllers/websocket.controller.ts
+++ src/controllers/websocket.controller.ts
@@ -1,19 +1,19 @@
-import { SocketStream } from '@fastify/websocket'
 import { FastifyRequest } from 'fastify'
+import { WebSocket } from 'ws'
 
-export const websocketHandler = async (connection: SocketStream, request: FastifyRequest) => {
+export const websocketHandler = async (socket: WebSocket, request: FastifyRequest) => {
 
     // set up socket listeners first
-    connection.socket.on('message', message => {
+    socket.on('message', message => {
         console.log(`Message from client ${client}: ${message.toString()}`)
     })
 
-    connection.socket.on('close', () => {
+    socket.on('close', () => {
         console.log(`Client ${client} disconnected.`)
     })
 
     // client logic
     const client = request.ip
 	console.log(`Client ${client} connected.`)
-    connection.socket.send(`Hello ${client}!`)
+    socket.send(`Hello ${client}!`)
 }
