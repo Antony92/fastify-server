17df44cf52459fbbb9826879ea5b076a503940a3
Antony92 <butterspower@gmail.com>
2026-01-20 10:15:22 +0200

    fixes

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -2,24 +2,24 @@ import { PrismaClient } from './prisma/client.js'
 import { PrismaPg } from '@prisma/adapter-pg'
 
 const adapter = new PrismaPg(
-  {
-    connectionString: process.env.DATABASE_URL!,
-  },
-  { schema: 'prisma' },
+	{
+		connectionString: process.env.DATABASE_URL!,
+	},
+	{ schema: 'prisma' },
 )
 
 const prisma = new PrismaClient({
-  adapter,
+	adapter,
 })
 
 export const isPrismaActive = async () => {
-  try {
-    await prisma.$queryRaw`SELECT now()`
-    return true
-  } catch (error) {
-    console.log(error)
-    return false
-  }
+	try {
+		await prisma.$queryRaw`SELECT now()`
+		return true
+	} catch (error) {
+		console.log(error)
+		return false
+	}
 }
 
 export default prisma
